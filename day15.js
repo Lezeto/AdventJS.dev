ceacf44c2ac6583dba2122a9a3228d9841251692
Lezeto <nicolas0vf@gmail.com>
2025-12-21 11:34:28 -0300

    day 15 basic version

---
+++ day15.js
@@ -0,0 +1,20 @@
+let lista=[{ name: 'Charlie', city: 'New York', age: 20 },
+    { name: 'Alice', city: 'London', age: 15 },
+    { name: 'Bob', city: 'Paris', age: 39 },
+    { name: 'Nicolas', city: 'Santiago', age: 30},
+    { name: 'Sandra', city: 'Santiago', age: 60},
+    { name: 'Harvey', city: 'New York', age: 34}];
+let lista2=[...lista];
+
+function sorting(data, sortBy){
+    let data2=[...data];
+    data2.sort((a,b)=>{ 
+        if(typeof(data[0][sortBy])==="string"){        
+            return a[sortBy].localeCompare(b[sortBy])}
+        if(typeof(data[0][sortBy])==="number"){
+            return a[sortBy]- b[sortBy]}
+    })
+    console.log(data2);
+}
+
+sorting(lista, "age");
